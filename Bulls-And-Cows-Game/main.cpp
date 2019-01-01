0c9ad0730925a444e0618a4a582e9a9c55a6c3dc
Casey O'Neill <caseyoneill78@hotmail.com>
2018-12-31 16:56:41 -0800

    Moving to Github

---
+++ Bulls-And-Cows-Game/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Bulls-And-Cows-Game
+//
+//  Created by Casey O'Neill on 12/31/18.
+//  Copyright © 2018 Context + Form Digital. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    cout << "Hello World!" << endl;
+    return 0;
+}
